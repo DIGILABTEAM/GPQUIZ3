386255fa67183bcf8c056ff3e564ce73b874453d
Vikas Gavandi <vekgraphics@gmail.com>
2021-06-22 17:17:47 +0530

    Update game.js

--- game.js
+++ game.js
@@ -11,7 +11,7 @@ let availablequestions = [];
 
 let questions = [
   {
-    question: "What can cause cough?",
+    question: "What can cause cough?                       ",
     choice1: "Allergies",
     choice2: "Acid reflux",
     choice3: "Secondhand smoke",
